063da3e99b6032f6ea41181996bc5714da122399
Senada Kadric <skadric@fordham.edu>
2020-04-30 13:53:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -32,14 +32,14 @@ hash = {
     
     {
       :player_name => "Brook Lopez",
-      :number => ,
-      :shoe => ,
-      :points => ,
-      :rebounds => ,
-      :assists => ,
-      :steals => ,
-      :blocks => ,
-      :slam_dunks => 
+      :number => 11,
+      :shoe => 17,
+      :points => 17,
+      :rebounds => 19,
+      :assists => 10,
+      :steals => 3,
+      :blocks => 1,
+      :slam_dunks => 15 
     },
     
     {
