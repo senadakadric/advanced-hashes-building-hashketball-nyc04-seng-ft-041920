31b761f2b1f10eed7953b00335df4222e987febe
Senada Kadric <skadric@fordham.edu>
2020-04-30 13:55:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -83,15 +83,15 @@ hash = {
       :slam_dunks => 2 
       }, 
       {
-        :player_name => ,
-      :number => ,
-      :shoe => ,
-      :points => ,
-      :rebounds => ,
-      :assists => ,
-      :steals => ,
-      :blocks => ,
-      :slam_dunks => 
+        :player_name => "Bismack Biyombo",
+      :number => 0,
+      :shoe => 16,
+      :points => 12,
+      :rebounds => 4,
+      :assists => 7,
+      :steals => 22,
+      :blocks => 15,
+      :slam_dunks => 10
       }, 
       {
         :player_name => ,
