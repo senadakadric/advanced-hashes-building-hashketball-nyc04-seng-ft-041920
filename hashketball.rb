c9b2f84601849a48b4580faf1065c122213a3205
Senada Kadric <skadric@fordham.edu>
2020-04-30 13:57:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -117,14 +117,14 @@ hash = {
       },
       {
       :player_name => "Kemba Walker",
-      :number => ,
-      :shoe => ,
-      :points => ,
-      :rebounds => ,
-      :assists => ,
-      :steals => ,
-      :blocks => ,
-      :slam_dunks => 
+      :number => 33,
+      :shoe => 15,
+      :points => 6,
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 7,
+      :blocks => 5,
+      :slam_dunks => 12
       }
       ]
   }
