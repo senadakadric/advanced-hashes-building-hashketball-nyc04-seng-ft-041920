91107aebb8e1ff21aed2b0b4bf1c0b0f616d6ddb
Senada Kadric <skadric@fordham.edu>
2020-04-30 13:40:20 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,28 @@
 # Write your code here!
 
 def game_hash
+ 
+hash = {
+  :home => {
+    :team_name => "",
+    :colors => ["", ""],
+    :players => {
+      
+    }
+  },
   
+  :away => {
+    :team_name => {
+      
+    },
+    :colors => {
+      
+    },
+    :players => {
+      
+    }
+  }
+
+} 
+ 
 end
